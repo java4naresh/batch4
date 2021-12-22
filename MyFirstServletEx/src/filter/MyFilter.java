bc1d896ac8eb8fba70390cd1f83b4f94e9369749
java4naresh <kambalanaresh4java@gmail.com>
2021-12-22 12:11:08 +0530

    adding my servlet example in to git

---
+++ MyFirstServletEx/src/filter/MyFilter.java
@@ -0,0 +1,68 @@
+package filter;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class MyFilter
+ */
+public class MyFilter implements Filter {
+	
+    /**
+     * Default constructor. 
+     */
+    public MyFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		/*HttpServletRequest hReq = (HttpServletRequest) request;
+		String name = hReq.getParameter("name");
+		Map<String, String[]> map = hReq.getParameterMap();
+		map.put("name", new String[]{name+" Kambala"});
+		hReq.setAttribute("name", name+" Kambala");*/
+		/*if(true) {
+			throw new RuntimeException("Unknown Source");
+		} else {
+		chain.doFilter(request, response);
+		}*/
+		/*long startTime = System.currentTimeMillis();
+		chain.doFilter(request, response);
+		long endTime = System.currentTimeMillis();
+		System.out.println("Request Processing Time is "+(endTime-startTime));*/
+		String regex = "[A-Za-z]";
+		HttpServletRequest hReq = (HttpServletRequest) request;
+		String name = hReq.getParameter("name");
+		if(null != name && !name.contains(regex)) {
+			throw new RuntimeException("Name Should Not contain numbers or speacial characters");
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		
+	}
+
+}
